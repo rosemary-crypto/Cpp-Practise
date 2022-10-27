5dd7c88502ff8179ec1dae54b7374a52b5f22f2e
rosemary-crypto <rosekoikara@gmail.com>
2022-10-27 12:27:50 +0900

    initial commit

---
+++ pointer_to_function/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string base {"hello world"};
+
+string append(string s)
+{
+    return base.append(" ").append(s);
+}
+
+string prepend(string s)
+{
+    return s.append(" ").append(base);
+}
+
+string combine(string s, string(*g)(string a))
+{
+    return (*g)(s);
+}
+
+int main()
+{
+    cout << combine("from MSVC", append) << "\n";
+    cout << combine("Good morning and", prepend) << "\n";
+}
